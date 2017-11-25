1cfcb936933041872a059f629dd30019a0440cef
Daniel Mooney <ddm7018@rit.edu>
2017-11-25 16:54:18 -0500

    adding surprise

--- surprise_map.R
+++ surprise_map.R
@@ -37,17 +37,62 @@ g <- list(
   lakecolor = toRGB('white')
 )
 for (ele in colnames(data)){
-p <- plot_geo(data, locationmode = 'USA-states') %>%
+print(plot_geo(data, locationmode = 'USA-states') %>%
   add_trace(
-    z = eval(parse(text= paste0("data$",ele))), locations = as.character(bb2state(data$State, convert = T)),
-    color = eval(parse(text= paste0("data$",ele))), colors = 'Reds'
-  ) %>%
-  colorbar(title = "Millions USD") %>%
+    z = eval(parse(text=paste0("data$",ele))), locations = as.character(bb2state(data$State, convert = T)),
+    color = eval(parse(text=paste0("data$",ele))), colors = 'Reds') %>%
   layout(
     title = ele,
     geo = g
-  )
-print(p)
+  ))
 }
+surpriseData <- data
+startYear <- 1981
 
+pMs = c(.333,.333,.333)
+pMDs = c()
+pDMs = c()
 
+boomYear <- data$X1998 
+bustYear <- data$X1981
+for(i in 0:17) {
+  year <- startYear + i
+  yearMean <- mean(eval(parse(text=paste0("data$X",year))))
+  yearSum  <- sum(eval(parse(text=paste0("data$X",year))))
+  yearData <- eval(parse(text=paste0("data$X",year)))
+  
+  for(i in 1:nrow(data)){
+    state <- eval(parse(text=paste0("data[i,]$X",year)))
+    diff1 <- (state/yearSum) - (yearMean/yearSum)
+    pDMs[1] = 1 - abs(diff1)
+    
+    diff2 =  (state/yearSum) - (boomYear[i]/yearSum)
+    pDMs[1] = 1 - abs(diff2)
+    
+    diff3 =  (state/yearSum) - (bustYear[i]/yearSum)
+    pDMs[1] = 1 - abs(diff3)
+    
+    pMDs[1] = pMs[1]*pDMs1
+    pMDs[2] = pMs[2]*pDMs2
+    pMDs[3] = pMs[3]*pDMs3
+    
+    kl = 0
+    voteSum = 0;
+    for(j in 1:length(pMDs)){
+      kl = kl + (pMDs[j] * (log( pMDs[j] / pMs[0])/log(2)))
+      voteSum  = voteSum + (diffs[j]*pMs[j])
+      sumDiffs[j] = sumDiffs[j] + (abs(diffs[j]))
+    }
+    #supriseData assignment is incorrect!
+    if(voteSum >= 0){
+      surpriseData[prop][i] <- abs(kl)
+    }
+    else{
+      surpriseData[prop][i] <- (-1* abs(kl))
+    }
+    
+  }
+  
+  }
+
+  
