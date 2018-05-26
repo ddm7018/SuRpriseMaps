597233f9f7b9931e6997b72f84d5fa3fd6657b39
Daniel Mooney <ddm7018@rit.edu>
2018-05-26 15:09:12 -0400

    adding leaflet support

--- app.R
+++ app.R
@@ -1,49 +1,51 @@
-
 library(shiny)
-library(shiny)
-library(shinythemes)      # Bootswatch color themes for shiny
-library(choroplethr)      # Creating Choropleth Maps in R
-library(choroplethrMaps)  # Maps used by the choroplethr package
-
-# load the data set from the choroplethrMaps package
-
+library(leaflet)
+library(rgdal)
 setwd("~/Documents/Projects/SuRpriseMaps")
+states   <- readOGR("cb_2017_us_state_20m/cb_2017_us_state_20m.shp", layer = "cb_2017_us_state_20m", GDAL1_integer64_policy = TRUE)
 map_data <- readRDS("surprise_map_data.RDS")
+states$region <- tolower(states$NAME)
+combo <- merge(states,map_data, on = "region")
 
-ui <- fluidPage(title = 'My First App!',
-                theme = shinythemes::shinytheme('flatly'),
-                titlePanel("Surprise Maps"),
-                sidebarLayout(
-                  sidebarPanel(width = 3,
-                               sliderInput("select_year",
-                                           label = "Select Year:",
-                                           min = 1981,
-                                           max = 1998,
-                                           value = 1981)
-                               ),
-                  
-                  mainPanel(width = 9, 
-                            tabsetPanel( 
-                              tabPanel(title = 'Output Map', 
-                                       plotOutput(outputId = "map")),
-                              tabPanel(title = 'Data Table', 
-                                       dataTableOutput(outputId = 'table'))))))
-
-server <- function(input, output) {
+ui <- fluidPage(
+  titlePanel("Hello Shiny!"),
+  sidebarLayout(
+    
+    sidebarPanel(
+      sliderInput("select_year",
+                  label = "Select Year:",
+                  min = 1981,
+                  max = 1998,
+                  value = 1981)
+    ),
+    
+    mainPanel(
+      leafletOutput("mymap")
+    )
+  )
   
-  output$map <- renderPlot({
+  
+  
+)
+
+server <- function(input, output, session) {
+
+  output$mymap <- renderLeaflet({
     
-    year <- paste0("X",input$select_year)
-    eval(parse(text = paste0("map_data$value <- map_data$",year)))
-    
-    state_choropleth(map_data,
-                     num_colors = 9)
+    leaflet() %>% addTiles()
   })
   
-  output$table <- renderDataTable({
-    
-    map_data
+  observe({
+    binpal <- colorBin("Blues", states$X1981, 6, pretty = FALSE)
+    eval(parse(text = paste0("leafletProxy('mymap', data = combo) %>%",
+                             "addPolygons(color = '#444444', weight = 1, smoothFactor = 0.5, opacity = 1.0,",
+                             "fillOpacity = 0.5, fillColor = ~binpal(X",input$select_year,"),",
+                             "highlightOptions = highlightOptions(color = 'white', weight = 2, bringToFront = TRUE)) %>%",
+                              " setView(lat = 41.489281, lng = -99.890151 , zoom = 4)")))
   })
+  
+  
 }
 
-shinyApp(ui,server)
+
+shinyApp(ui, server)
